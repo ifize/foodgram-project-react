7efd261c958eaf65ffc810afa2caa5107c667350
Илья Воронков <voronkovid@gmail.com>
2022-10-28 19:14:58 +0300

    debug=True

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.9.25', 'localhost', 'backend', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['158.160.9.25', 'localhost', 'backend', '127.0.0.1:8000', '127.0.0.1']
 
 
 # Application definition
